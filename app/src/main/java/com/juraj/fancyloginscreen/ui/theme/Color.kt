855f7c85dda75c4cbaac719fd89bb8691bce0cf7
Juraj Kusnier <jurajkusnier@gmail.com>
2022-03-14 14:20:11 +0100

    Initial commit

---
+++ app/src/main/java/com/juraj/fancyloginscreen/ui/theme/Color.kt
@@ -0,0 +1,7 @@
+package com.juraj.fancyloginscreen.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val LightBlue800 = Color(0xFF0277bd)
+val LightBlue500 = Color(0xFF58a5f0)
+val LightBlue900 = Color(0xFF004c8c)
---
+++ app/src/main/java/com/juraj/fancyloginscreen/ui/theme/Theme.kt
@@ -0,0 +1,36 @@
+package com.juraj.fancyloginscreen.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+private val LightColorPalette = lightColors(
+    primary = LightBlue800,
+    primaryVariant = LightBlue500,
+    secondary = LightBlue900,
+    background = Color.DarkGray,
+
+    /* Other default colors to override
+    surface = Color.White,
+    onPrimary = Color.White,
+    onSecondary = Color.Black,
+    onBackground = Color.Black,
+    onSurface = Color.Black,
+    */
+)
+
+@Composable
+fun FancyLoginScreenTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+
+    MaterialTheme(
+        colors = LightColorPalette,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
